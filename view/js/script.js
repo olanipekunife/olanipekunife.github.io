22170874e531232bb5df7e17d759f716bfc8f620
Nipek <olanipekunifeoluwao@gmail.com>
2019-03-10 05:15:36 +0100

    .

--- view/js/script.js
+++ view/js/script.js
@@ -1,7 +1,6 @@
 var lnStickyNavigation;
 
-$(document).ready(function()
-{
+$(document).ready(function () {
 	applyHeader();
 	applyNavigation();
 	applyMailTo();
@@ -12,131 +11,110 @@ $(document).ready(function()
 
 /* HEADER FUNCTIONS */
 
-function applyHeader()
-{
-	$('.jumbotron').css({ height: ($(window).height()) +'px' });
+function applyHeader() {
+	$('.jumbotron').css({
+		height: ($(window).height()) + 'px'
+	});
 
 
 }
 
-function lazyLoad(poContainer)
-{
+function lazyLoad(poContainer) {
 
 }
 
 /* NAVIGATION FUNCTIONS */
 
-function applyNavigation()
-{
+function applyNavigation() {
 	applyClickEvent();
 	applyNavigationFixForPhone();
 	applyScrollSpy();
 	applyStickyNavigation();
 }
 
-function applyClickEvent()
-{
-	$('a[href*=#]').on('click', function(e)
-	{
+function applyClickEvent() {
+	$('a[href*=#]').on('click', function (e) {
 		e.preventDefault();
 
-		if( $( $.attr(this, 'href') ).length > 0 )
-		{
-			$('html, body').animate(
-			{
-				scrollTop: $( $.attr(this, 'href') ).offset().top
+		if ($($.attr(this, 'href')).length > 0) {
+			$('html, body').animate({
+				scrollTop: $($.attr(this, 'href')).offset().top
 			}, 400);
 		}
 		return false;
 	});
 }
 
-function applyNavigationFixForPhone()
-{
-	$('.navbar li a').click(function(event)
-	{
+function applyNavigationFixForPhone() {
+	$('.navbar li a').click(function (event) {
 		$('.navbar-collapse').removeClass('in').addClass('collapse');
 	});
 }
 
-function applyScrollSpy()
-{
-	$('#navbar-example').on('activate.bs.scrollspy', function()
-	{
+function applyScrollSpy() {
+	$('#navbar-example').on('activate.bs.scrollspy', function () {
 		window.location.hash = $('.nav .active a').attr('href').replace('#', '#/');
 	});
 }
 
-function applyStickyNavigation()
-{
+function applyStickyNavigation() {
 	lnStickyNavigation = $('.scroll-down').offset().top + 20;
 
-	$(window).on('scroll', function()
-	{
+	$(window).on('scroll', function () {
 		stickyNavigation();
 	});
 
 	stickyNavigation();
 }
 
-function stickyNavigation()
-{
-	if($(window).scrollTop() > lnStickyNavigation)
-	{
+function stickyNavigation() {
+	if ($(window).scrollTop() > lnStickyNavigation) {
 		$('body').addClass('fixed');
-	}
-	else
-	{
+	} else {
 		$('body').removeClass('fixed');
 	}
 }
 
 /* MAILTO FUNCTION */
 
-function applyMailTo()
-{
-	$('a[href*=mailto]').on('click', function(e)
-	{
+function applyMailTo() {
+	$('a[href*=mailto]').on('click', function (e) {
 		var lstrEmail = $(this).attr('href').replace('mailto:', '');
 
 		lstrEmail = lstrEmail.split('').reverse().join('')
 
-		$(this).attr('href', 'mailto:hotcomputerworks@hotmail.com');
+		$(this).attr('href', 'mailto:olanipekunifeoluwao@gmail.com');
 	});
 }
 
 /* RESIZE FUNCTION */
 
-function applyResize()
-{
-	$(window).on('resize', function()
-	{
+function applyResize() {
+	$(window).on('resize', function () {
 		lnStickyNavigation = $('.scroll-down').offset().top + 20;
 
-		$('.jumbotron').css({ height: ($(window).height()) +'px' });
+		$('.jumbotron').css({
+			height: ($(window).height()) + 'px'
+		});
 	});
 }
 
 /* HASH FUNCTION */
 
-function checkHash()
-{
+function checkHash() {
 	lstrHash = window.location.hash.replace('#/', '#');
 
-	if($('a[href='+ lstrHash +']').length > 0)
-	{
-		$('a[href='+ lstrHash +']').trigger('click');
+	if ($('a[href=' + lstrHash + ']').length > 0) {
+		$('a[href=' + lstrHash + ']').trigger('click');
 	}
 }
 
 /* IE7- FALLBACK FUNCTIONS */
 
-function checkBrowser()
-{
+function checkBrowser() {
 	var loBrowserVersion = getBrowserAndVersion();
 
-	if(loBrowserVersion.browser == 'Explorer' && loBrowserVersion.version < 8)
-	{
+	if (loBrowserVersion.browser == 'Explorer' && loBrowserVersion.version < 8) {
 		$('#upgrade-dialog').modal({
 			backdrop: 'static',
 			keyboard: false
@@ -144,13 +122,12 @@ function checkBrowser()
 	}
 }
 
-function getBrowserAndVersion()
-{
+function getBrowserAndVersion() {
 	var laBrowserData = [{
-		string: 		navigator.userAgent,
-		subString: 		'MSIE',
-		identity: 		'Explorer',
-		versionSearch: 	'MSIE'
+		string: navigator.userAgent,
+		subString: 'MSIE',
+		identity: 'Explorer',
+		versionSearch: 'MSIE'
 	}];
 
 	return {
@@ -159,35 +136,27 @@ function getBrowserAndVersion()
 	};
 }
 
-function searchString(paData)
-{
-	for(var i = 0; i < paData.length; i++)	
-	{
-		var lstrDataString 	= paData[i].string;
-		var lstrDataProp 	= paData[i].prop;
+function searchString(paData) {
+	for (var i = 0; i < paData.length; i++) {
+		var lstrDataString = paData[i].string;
+		var lstrDataProp = paData[i].prop;
 
 		this.versionSearchString = paData[i].versionSearch || paData[i].identity;
 
-		if(lstrDataString)
-		{
-			if(lstrDataString.indexOf(paData[i].subString) != -1)
-			{
+		if (lstrDataString) {
+			if (lstrDataString.indexOf(paData[i].subString) != -1) {
 				return paData[i].identity;
 			}
-		}
-		else if(lstrDataProp)
-		{
+		} else if (lstrDataProp) {
 			return paData[i].identity;
 		}
 	}
 }
 
-function searchVersion(pstrDataString)
-{
+function searchVersion(pstrDataString) {
 	var lnIndex = pstrDataString.indexOf(this.versionSearchString);
 
-	if(lnIndex == -1)
-	{
+	if (lnIndex == -1) {
 		return;
 	}
 
